427ee89fc1f6cd481aa009e48cf4867d49b501b3
Nadav <nadav.tzabari@amdocs.com>
2020-11-29 23:59:48 +0200

    Added UserRating api

---
+++ src/main/java/movies/server/ratingdataservice/models/UserRating.java
@@ -0,0 +1,13 @@
+package movies.server.ratingdataservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class UserRating {
+
+    private List<Rating> rating ;
+}
--- src/main/java/movies/server/ratingdataservice/resources/RatingResource.java
+++ src/main/java/movies/server/ratingdataservice/resources/RatingResource.java
@@ -1,16 +1,29 @@
 package movies.server.ratingdataservice.resources;
 
 import movies.server.ratingdataservice.models.Rating;
+import movies.server.ratingdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingResource {
 
-    @RequestMapping("/{movieId}")
-    public Rating getRating(@PathVariable String movieId) {
+    @RequestMapping(value = "/{movieId}" , method = RequestMethod.GET)
+    public Rating getMovieRating(@PathVariable String movieId) {
         return new Rating(movieId,4);
+    }
+
+    @RequestMapping(value = "users/{userId}" , method = RequestMethod.GET)
+    public UserRating getUserRating(@PathVariable String userId) {
+        return new UserRating(Arrays.asList(
+                new Rating("34321", 3),
+                new Rating("554321", 7),
+                new Rating("621", 2)
+        ));
     }
 }
